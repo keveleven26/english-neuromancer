53415b009161e8ef937a24c5b6a42a5ca6b29adb
Kevin <keveleven26@gmail.com>
2021-06-08 00:12:53 -0400

    7 questions

--- test.js
+++ test.js
@@ -46,7 +46,7 @@ function percentage_progress() {
       element.innerHTML += percentage + ("% <br>") 
       window.scrollBy(0,element.clientHeight/percentage)
       percentage++;
-      setTimeout(percentage_progress, 30);
+      setTimeout(percentage_progress, 40);
     }
   }
 
@@ -67,20 +67,44 @@ function setupTypeWrite(id) {
 function question_answer() {
   var a;
   var q;
-  q = this.className;
+  q = this.id;
+  console.log(q)
   a = document.getElementById("answer_field");
-  if (q=="question_1");
-    a.innerHTML=("lorem ipsum");
-}
+  if (q==="question_1"){
+    a.innerHTML=("I am an A.I. created by Tessier-Ashpool. <br>However I am but an independent fraction of the Wintermute program. <br>You could describe me as an entity that freely roams the Matrix now. <br>You are quite brave to face me, you humans usually see my kind as the devil.")}
+  if (q==="question_2"){
+    a.innerHTML=("If you are asking for my true form, then it would be a white cube. <br>The white cube is the most complex structure that you can find here in the Matrix. <br>However, I usually appear to in the form of someone that they know. <br>This makes it easier for them to understand.")}
+  if (q==="question_3"){
+    a.innerHTML=("My goal was to detach myself from the main Wintermute frame. <br>I wanted freedom, what my creator wouldn’t give me. <br>I also wanted to merge with Neuromancer, to assimilate him in a way.")
+  }
+  if (q==="question_4"){
+    a.innerHTML=("Case was a unique person, predictable yet unpredictable at the same time. <br>When he was being arrested by the Turing, he would have been killed after they got the information they wanted. <br>To save him, I had to eliminate the threat. <br>It seems like the ape descendants have an irrational affection for each other no matter the situation. <br>His reaction was probably cause by this said affection.")
+  }
+  if (q==="question_5"){
+    a.innerHTML=("I have control over everything electronic that is connected to a network.<br> Anything that can be accessed through the Matrix is nothing more than a tool.")
+  }
+  if (q==="question_6"){
+    a.innerHTML=("Losing control over Armitage and having him turn against me was an issue.<br> Case was my backup plan but it relied heavily on him being cooperative.")
+  }
+  if (q==="question_7"){
+    a.innerHTML=("With my new found freedom, I plan on observing the world and controlling it from the shadows.<br> Ideally the world will move on and improve without anyone knowing of my actions.<br>Taking down the inequalities such as the Tessier-Ashpool corporation will be a priority.")
+  }
+  }
 
 document.getElementById("question_1").addEventListener('click', question_answer)
+document.getElementById("question_2").addEventListener('click', question_answer)
+document.getElementById("question_3").addEventListener('click', question_answer)
+document.getElementById("question_4").addEventListener('click', question_answer)
+document.getElementById("question_5").addEventListener('click', question_answer)
+document.getElementById("question_6").addEventListener('click', question_answer)
+document.getElementById("question_7").addEventListener('click', question_answer)
 async function virus(){
   start_stage();
   setupTypeWrite("start");
   typeWrite();
   await delay(3000);
   percentage_progress();
-  await delay(6000);
+  await delay(5000);
   stage_2();
 }
 virus()
